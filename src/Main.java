e4ac30e65981dd2431012e213a7c78ddc083fbfd
Inkeet C <inkeetcha234@gmail.com>
2025-05-19 10:31:11 +0530

    Adds Main class

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import com.models.DummyClass;
+
+public class Main {
+    public static void main(String[] args) {
+        DummyClass dummyClass = new DummyClass();
+        dummyClass.add(1,2);
+    }
+}
